b01a6e53c4098ae2c21bff92a6a02d58375084aa
TuanAnhTran2k5 <tranvantuananh01012005@gmail.com>
2026-06-17 00:20:33 +0700

    add AppSidebar component with menu items and routing

--- src/configs/router.tsx
+++ src/configs/router.tsx
@@ -2,10 +2,11 @@ import HomePage from "@/pages/HomePage";
 import LoginPage from "@/pages/LoginPage";
 import RegisterPage from "@/pages/RegisterPage";
 import AuthLayout from "@/layouts/AuthLayout";
-import MainLayout from "@/layouts/MainLayout";
+import MainLayout from "@/layouts/PublicLayout";
 import { ROUTE } from "@/models/routePath";
 import { createBrowserRouter } from "react-router-dom";
 import DocumentsPage from "@/pages/DocumentsPage";
+import UserLayout from "@/layouts/UserLayout";
 
 export const router = createBrowserRouter([
   {
@@ -27,7 +28,7 @@ export const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       {
-        path: "",
+        index: true,
         element: <HomePage />,
       },
       {
@@ -36,9 +37,63 @@ export const router = createBrowserRouter([
       },
     ],
   },
+  {
+    path: ROUTE.APP,
+    element: <UserLayout />,
+    children: [
+      {
+        index: true,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.DASHBOARD,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.MY_DOCUMENTS,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.UPLOAD_DOCUMENT,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.COMMUNITY,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.AI_CHAT,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.BOOKMARKS,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.NOTIFICATIONS,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.LEADERBOARD,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.PROFILE,
+        element: <div> </div>,
+      },
+      {
+        path: ROUTE.SETTINGS,
+        element: <div> </div>,
+      },
+    ],
+  },
 ]);
 
 export const NAVIGATE_KEY = [
+  {
+    name: "Home",
+    path: ROUTE.HOME,
+  },
   {
     name: "Document",
     path: ROUTE.DOCUMENTS,
@@ -47,31 +102,12 @@ export const NAVIGATE_KEY = [
     name: "Community",
     path: <div></div>,
   },
-  {
-    name: "AI Chat",
-    path: <div></div>,
-  },
-  {
-    name: "Leaderboard",
-    path: <div></div>,
-  },
+  // {
+  //   name: "AI Chat",
+  //   path: <div></div>,
+  // },
+  // {
+  //   name: "Leaderboard",
+  //   path: <div></div>,
+  // },
 ];
-
-// export const NAVIGATE_KEY = [
-//   {
-//     name: "Document",
-//     path: "/documents",
-//   },
-//   {
-//     name: "Community",
-//     path: "/community",
-//   },
-//   {
-//     name: "AI Chat",
-//     path: "/chat",
-//   },
-//   {
-//     name: "Leaderboard",
-//     path: "/leaderboard",
-//   },
-// ];
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 
 function AuthLayout() {
   return (
+     // Login, Register
     <main className="flex min-h-screen flex-col bg-background">
       <Header />
 
